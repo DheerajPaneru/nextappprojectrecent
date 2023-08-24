a25536056d95048b6d56847d2db1abbaf718c4f6
Dheeraj Paneru <dheerajpaneru8@gmail.com>
2023-08-25 01:09:18 +0530

    next app SECOND ASSIGNMENThj

--- src/app/context/page.js
+++ src/app/context/page.js
@@ -1,5 +1,5 @@
 "use client"
-import { createContext, useState, useContext, useEffect } from 'react';
+import React,{ createContext, useState, useContext, useEffect } from 'react';
 
 const AuthContext = createContext();
 
@@ -14,18 +14,9 @@ export function AuthProvider({ children }) {
   }, []);
 
   const login = (userData) => {
-    // Simulate a login request to a server
-    // In a real app, this would be an API call
     setUser(userData);
     localStorage.setItem('user', JSON.stringify(userData));
   };
-
-  const logout = () => {
-    // Simulate a logout by removing user data from state and local storage
-    setUser(null);
-    localStorage.removeItem('user');
-  };
-
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
